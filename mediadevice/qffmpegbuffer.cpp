5990a71f1b33159fd5318364e9c9cd5c36cc871c
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2017-11-27 22:02:29 +0100

    buffer for qffmpeg encoding

--- mediadevice/qffmpegbuffer.cpp
+++ mediadevice/qffmpegbuffer.cpp
@@ -1,6 +1,80 @@
 #include "qffmpegbuffer.h"
 
+qint64 QFfmpegBuffer::objectCounter = 0;
+
 QFfmpegBuffer::QFfmpegBuffer()
 {
+    objectCounter++;
 
+    avio_ctx_buffer = (uint8_t*)av_malloc(avio_ctx_buffer_size);
+    if (avio_ctx_buffer != NULL)
+    {
+        avio_ctx = avio_alloc_context(avio_ctx_buffer, avio_ctx_buffer_size,
+                                      1, &buffer, &QFfmpegBuffer::read_packet, &QFfmpegBuffer::write_packet, NULL);
+    }
+}
+
+QFfmpegBuffer::~QFfmpegBuffer()
+{
+    objectCounter--;
+
+    if (avio_ctx != NULL)
+    {
+        av_freep(&avio_ctx->buffer);
+        av_freep(&avio_ctx);
+    }
+}
+
+bool QFfmpegBuffer::isValid() const
+{
+    return avio_ctx_buffer != NULL && avio_ctx != NULL;
+}
+
+AVIOContext *QFfmpegBuffer::context() const
+{
+    return avio_ctx;
+}
+
+bool QFfmpegBuffer::atEnd() const
+{
+    return bytesAvailable() == 0;
+}
+
+qint64 QFfmpegBuffer::bytesAvailable() const
+{
+    return buffer.size();
+}
+
+QByteArray QFfmpegBuffer::read(const int &maxlen)
+{
+    qint64 bytesToRead = qMin(buffer.size(), maxlen);
+    QByteArray res = buffer.left(bytesToRead);
+    buffer.remove(0, res.size());
+    return res;
+}
+
+int QFfmpegBuffer::read_packet(void *opaque, uint8_t *buf, int buf_size)
+{
+    QByteArray *bd = (QByteArray *)opaque;
+
+    /* copy internal buffer data to buf */
+    memcpy(buf, bd->data(), buf_size);
+    bd->remove(0, buf_size);
+
+    return buf_size;
+}
+
+int QFfmpegBuffer::write_packet(void *opaque, uint8_t *buf, int buf_size)
+{
+    QByteArray *bd = (QByteArray *)opaque;
+
+    bd->append(QByteArray::fromRawData((char *)buf, buf_size));
+
+    return buf_size;
+}
+
+void QFfmpegBuffer::flush()
+{
+    if (avio_ctx != NULL)
+        avio_flush(avio_ctx);
 }
--- mediadevice/qffmpegbuffer.h
+++ mediadevice/qffmpegbuffer.h
@@ -1,11 +1,45 @@
 #ifndef QFFMPEGBUFFER_H
 #define QFFMPEGBUFFER_H
 
+#include <QDebug>
+
+extern "C" {
+#include <libavformat/avio.h>
+}
 
 class QFfmpegBuffer
 {
+
+    struct buffer_data {
+        uint8_t *ptr;
+        int size; ///< size left in the buffer
+    };
+
 public:
     QFfmpegBuffer();
+    virtual ~QFfmpegBuffer();
+
+    bool isValid() const;
+
+    AVIOContext *context() const;
+
+    bool atEnd() const;
+    qint64 bytesAvailable() const;
+    QByteArray read(const int &maxlen);
+    void flush();
+
+private:
+    static int read_packet(void *opaque, uint8_t *buf, int buf_size);
+    static int write_packet(void *opaque, uint8_t *buf, int buf_size);
+
+public:
+    static qint64 objectCounter;
+
+private:
+    AVIOContext *avio_ctx = NULL;
+    uint8_t *avio_ctx_buffer = NULL;
+    size_t avio_ctx_buffer_size = 4096;
+    QByteArray buffer;
 };
 
 #endif // QFFMPEGBUFFER_H
