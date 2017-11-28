01e6e6ae9d75a4f6829826a36715a7747b946f12
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2017-11-28 22:05:13 +0100

    qffmpeg media

--- mediadevice/qffmpegmedia.cpp
+++ mediadevice/qffmpegmedia.cpp
@@ -1,6 +1,192 @@
 #include "qffmpegmedia.h"
 
+bool QFfmpegMedia::m_initialised = false;
+
+qint64 QFfmpegMedia::objectCounter = 0;
+
 QFfmpegMedia::QFfmpegMedia()
 {
+    objectCounter++;
 
+    init();
+}
+
+QFfmpegMedia::~QFfmpegMedia()
+{
+    objectCounter--;
+}
+
+void QFfmpegMedia::init()
+{
+
+#ifdef QT_DEBUG
+    av_log_set_level(AV_LOG_DEBUG);
+#else
+    av_log_set_level(AV_LOG_ERROR);
+#endif
+
+    if (!m_initialised)
+    {
+        av_register_all();
+        m_initialised = true;
+    }
+}
+
+QString QFfmpegMedia::filename() const
+{
+    QString res;
+
+    if (context() != NULL)
+        res = QString(context()->filename);
+
+    return res;
+}
+
+bool QFfmpegMedia::isValid() const
+{
+    return m_initialised && m_error.isEmpty();
+}
+
+void QFfmpegMedia::setParent(void *parent)
+{
+    m_parent = parent;
+}
+
+void *QFfmpegMedia::parent() const
+{
+    return m_parent;
+}
+
+void QFfmpegMedia::setError(const QString &error)
+{
+    m_error = error;
+}
+
+QString QFfmpegMedia::getVersion()
+{
+    return QString("libavformat %1.%2, libavcodec %3.%4").arg(LIBAVFORMAT_VERSION_MAJOR).arg(LIBAVFORMAT_VERSION_MINOR).arg(LIBAVCODEC_VERSION_MAJOR).arg(LIBAVCODEC_VERSION_MINOR);
+}
+
+bool QFfmpegMedia::isOpen()
+{
+    if (context() != NULL)
+        return context()->pb != NULL;
+    else
+        return false;
+}
+
+QStringList QFfmpegMedia::getMetaDataStream(const AVMediaType &type, const QString &param) const
+{
+    QStringList res;
+
+    if (context() != NULL)
+    {
+        for (unsigned int i=0; i<context()->nb_streams; ++i)
+        {
+            AVStream *stream = context()->streams[i];
+            if (stream != NULL && stream->codecpar != NULL && stream->codecpar->codec_type == type)
+            {
+                bool tagFound = false;
+                AVDictionaryEntry *entry = NULL;
+                while ((entry = av_dict_get(stream->metadata, "", entry, AV_DICT_IGNORE_SUFFIX)))
+                {
+                    if (entry->key == param)
+                    {
+                        res << entry->value;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if (!tagFound)
+                    res << "";
+            }
+        }
+    }
+
+    return res;
+}
+
+QStringList QFfmpegMedia::getAudioLanguages() const
+{
+   return getMetaDataStream(AVMEDIA_TYPE_AUDIO, "language");
+}
+
+QStringList QFfmpegMedia::getVideoLanguages() const
+{
+    return getMetaDataStream(AVMEDIA_TYPE_VIDEO, "language");
+}
+
+QStringList QFfmpegMedia::getSubtitleLanguages() const
+{
+    return getMetaDataStream(AVMEDIA_TYPE_SUBTITLE, "language");
+}
+
+QString QFfmpegMedia::metaData(const QString &tagName) const
+{
+    if (context() != NULL)
+    {
+        AVDictionaryEntry *entry = NULL;
+        while ((entry = av_dict_get(context()->metadata, "", entry, AV_DICT_IGNORE_SUFFIX)))
+        {
+            if (entry != NULL && entry->key == tagName)
+                return  entry->value;
+        }
+    }
+
+    return QString();
+}
+
+QHash<QString, double> QFfmpegMedia::getVolumeInfo(const int timeout)
+{
+    QHash<QString, double> res;
+    return res;
+}
+
+QString QFfmpegMedia::getAudioFormat() const
+{
+    if (audioStream())
+        return audioStream()->format();
+    else
+        return QString();
+}
+
+int QFfmpegMedia::getAudioChannelCount() const
+{
+    if (audioStream())
+        return audioStream()->channelCount();
+    else
+        return -1;
+}
+
+int QFfmpegMedia::getAudioSamplerate() const
+{
+    if (audioStream())
+        return audioStream()->samplerate();
+    else
+        return -1;
+}
+
+qint64 QFfmpegMedia::getAudioBitrate() const
+{
+    if (audioStream())
+        return audioStream()->bitrate();
+    else
+        return -1;
+}
+
+QString QFfmpegMedia::getVideoResolution() const
+{
+    if (videoStream())
+        return videoStream()->resolution();
+    else
+        return QString();
+}
+
+double QFfmpegMedia::getVideoFrameRate() const
+{
+    if (videoStream())
+        return videoStream()->frameRate();
+    else
+        return 0.0;
 }
--- mediadevice/qffmpegmedia.h
+++ mediadevice/qffmpegmedia.h
@@ -1,11 +1,74 @@
 #ifndef QFFMPEGMEDIA_H
 #define QFFMPEGMEDIA_H
 
+#include <QDebug>
+#include "qffmpegstream.h"
 
 class QFfmpegMedia
 {
+
 public:
     QFfmpegMedia();
+    virtual ~QFfmpegMedia();
+
+    static QString getVersion();
+
+    virtual AVFormatContext *context() const = 0;
+
+    QString filename() const;
+
+    bool isValid() const;
+
+    void setParent(void *parent);
+    void *parent() const;
+
+    void setError(const QString &error);
+
+    virtual bool close() = 0;
+    bool isOpen();
+
+    virtual QFfmpegStream *audioStream() const = 0;
+    virtual QFfmpegStream *videoStream() const = 0;
+    virtual QFfmpegStream *subtitleStream() const = 0;
+
+    virtual qint64 size() const = 0;
+    virtual QString getFormat() const = 0;
+    virtual qint64 getDuration() const = 0;
+    virtual qint64 getBitrate() const = 0;
+
+    QString getAudioFormat() const;
+    int getAudioChannelCount() const;
+    int getAudioSamplerate() const;
+    qint64 getAudioBitrate() const;
+
+    QString getVideoResolution() const;
+    double getVideoFrameRate() const;
+
+    QHash<QString, double> getVolumeInfo(const int timeout = 30000);
+
+    QStringList getAudioLanguages() const;
+    QStringList getVideoLanguages() const;
+    QStringList getSubtitleLanguages() const;
+
+    QString metaData(const QString &tagName) const;
+
+    virtual bool atEnd() const = 0;
+
+    virtual void setTimeEndInMsec(const qint64 &msec) = 0;
+    virtual qint64 timeEndInMsec() const = 0;
+
+private:
+    void init();
+
+    QStringList getMetaDataStream(const AVMediaType &type, const QString &param) const;
+
+public:
+    static qint64 objectCounter;
+
+private:
+    void *m_parent = Q_NULLPTR;
+    static bool m_initialised;
+    QString m_error;
 };
 
 #endif // QFFMPEGMEDIA_H
