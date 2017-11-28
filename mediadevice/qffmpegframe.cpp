c831999873e252724c3197737fbf8020b9c8d080
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2017-11-28 19:54:01 +0100

    ffmpeg frame

--- mediadevice/qffmpegframe.cpp
+++ mediadevice/qffmpegframe.cpp
@@ -1,6 +1,62 @@
 #include "qffmpegframe.h"
 
-QFfmpegFrame::QFfmpegFrame()
-{
+qint64 QFfmpegFrame::objectCounter = 0;
 
+QFfmpegFrame::QFfmpegFrame():
+    m_frame(av_frame_alloc())
+{
+    objectCounter++;
+}
+
+QFfmpegFrame::~QFfmpegFrame()
+{
+    objectCounter--;
+
+    if (isValid())
+        av_frame_free(&m_frame);
+}
+
+bool QFfmpegFrame::isValid() const
+{
+    return m_frame != NULL;
+}
+
+AVFrame *QFfmpegFrame::ptr() const
+{
+    return m_frame;
+}
+
+void QFfmpegFrame::setMediaType(const AVMediaType &type)
+{
+    m_mediaType = type;
+}
+
+AVMediaType QFfmpegFrame::mediaType() const
+{
+    return m_mediaType;
+}
+
+bool QFfmpegFrame::init_frame(AVSampleFormat format, uint64_t channel_layout, int sample_rate, int nb_samples)
+{
+    if (m_frame != NULL)
+    {
+        m_frame->format = format;
+        m_frame->channel_layout = channel_layout;
+        m_frame->sample_rate = sample_rate;
+        m_frame->nb_samples = nb_samples;
+
+        if (av_frame_get_buffer(m_frame, 0) < 0)
+        {
+            qCritical() << "Error allocation frame buffer.";
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    else
+    {
+        return false;
+    }
 }
--- mediadevice/qffmpegframe.h
+++ mediadevice/qffmpegframe.h
@@ -1,11 +1,33 @@
 #ifndef QFFMPEGFRAME_H
 #define QFFMPEGFRAME_H
 
+#include <QDebug>
+
+extern "C" {
+#include <libavutil/frame.h>
+}
 
 class QFfmpegFrame
 {
 public:
     QFfmpegFrame();
+    virtual ~QFfmpegFrame();
+
+    bool isValid() const;
+
+    AVFrame *ptr() const;
+
+    bool init_frame(AVSampleFormat format, uint64_t channels, int sample_rate, int nb_samples);
+
+    void setMediaType(const AVMediaType &type);
+    AVMediaType mediaType() const;
+
+public:
+    static qint64 objectCounter;
+
+private:
+    AVFrame *m_frame = NULL;
+    AVMediaType m_mediaType = AVMEDIA_TYPE_UNKNOWN;
 };
 
 #endif // QFFMPEGFRAME_H
