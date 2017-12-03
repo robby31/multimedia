09462f2fda3ccf5672750e3c5c04c8c9a219edbc
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2017-12-03 19:19:22 +0100

    ffmpeg packet

--- mediadevice/qffmpegpacket.cpp
+++ mediadevice/qffmpegpacket.cpp
@@ -1,6 +1,27 @@
 #include "qffmpegpacket.h"
 
-QFfmpegPacket::QFfmpegPacket()
-{
+qint64 QFfmpegPacket::objectCounter = 0;
 
+QFfmpegPacket::QFfmpegPacket():
+    m_pkt(av_packet_alloc())
+{
+    objectCounter++;
+}
+
+QFfmpegPacket::~QFfmpegPacket()
+{
+    objectCounter--;
+
+    if (isValid())
+        av_packet_free(&m_pkt);
+}
+
+bool QFfmpegPacket::isValid() const
+{
+    return m_pkt != NULL;
+}
+
+AVPacket *QFfmpegPacket::ptr() const
+{
+    return m_pkt;
 }
--- mediadevice/qffmpegpacket.h
+++ mediadevice/qffmpegpacket.h
@@ -1,11 +1,26 @@
 #ifndef QFFMPEGPACKET_H
 #define QFFMPEGPACKET_H
 
+#include "qglobal.h"
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
 
 class QFfmpegPacket
 {
 public:
     QFfmpegPacket();
+    virtual ~QFfmpegPacket();
+
+    bool isValid() const;
+    AVPacket *ptr() const;
+
+public:
+    static qint64 objectCounter;
+
+private:
+    AVPacket *m_pkt = NULL;
 };
 
 #endif // QFFMPEGPACKET_H
